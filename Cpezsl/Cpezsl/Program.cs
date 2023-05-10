f324ca6226ea058fabacf35a543ba4ca88d59661
Mariaxuf22 <97722846+Mariaxuf22@users.noreply.github.com>
2023-05-10 11:03:34 +0300

    Add files via upload

---
+++ Cpezsl/Cpezsl/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpezsl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вберите специальную службу из списка");
+            Console.WriteLine("1. МЧС");
+            Console.WriteLine("2. Полиция");
+            Console.WriteLine("3. Скорая помощь");
+            Console.WriteLine("4. Аварийная газовая служба");
+            Console.WriteLine("5. Выход");
+            var keyInfo = Console.ReadKey(true);
+            switch(keyInfo.KeyChar)
+            {
+                case '1':
+                    Console.WriteLine("Вы выбрали МЧС");
+                    break;
+                case '2':
+                    Console.WriteLine("Вы выбрали полицию");
+                    break;
+                case '3':
+                    Console.WriteLine("Вы выбрали скорую помощь");
+                    break;
+                case '4':
+                    Console.WriteLine("Вы выбрали аварийную газовую службу");
+                break;
+                case '5':
+                    Console.WriteLine("Вы точно хотите выйти из меню?");
+                    if (keyInfo.KeyChar == 'y')
+                        Console.Clear();
+
+                    break;
+
+            }
+            
+        }
+
+    }
+}
